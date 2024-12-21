bda18a957829de695dc62c519e62255dbf17f4bd
Juntao Qiu <juntao.qiu@gmail.com>
2024-12-21 16:06:06 +1100

    extract the quotes into an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
-import {useEffect, useRef} from "react";
+import "./App.css";
+import { useEffect, useRef } from "react";
+
+const quotes = [
+  "The only limit to our realization of tomorrow will be our doubts of today. – Franklin D. Roosevelt",
+  "Do what you can, with what you have, where you are. – Theodore Roosevelt",
+  "Don’t count the days, make the days count. – Muhammad Ali",
+  "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis",
+  "Success is walking from failure to failure with no loss of enthusiasm. – Winston Churchill",
+  "It does not matter how slowly you go as long as you do not stop. – Confucius",
+  "What lies behind us and what lies before us are tiny matters compared to what lies within us. – Ralph Waldo Emerson",
+  "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+  "Strive not to be a success, but rather to be of value. – Albert Einstein",
+  "Dream big and dare to fail. – Norman Vaughan",
+];
 
 function App() {
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if(ref.current === null) {
+    if (ref.current === null) {
       return;
     }
 
@@ -15,22 +28,27 @@ function App() {
     };
 
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if(entry.isIntersecting) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
           const element = entry.target;
           console.log(element.innerHTML);
-          observer.unobserve(element);
-          if(element.className.includes("last")) {
-            console.log('loading more quotes from server');
+
+          if (element.className.includes("trigger")) {
+            console.log("loading more quotes from server");
           }
+
+          observer.unobserve(element);
         }
-      })
+      });
     }, options);
 
-    const listItems = document.querySelectorAll('.list-item');
-    listItems.forEach(item => {
+    const listItems = document.querySelectorAll(".list-item");
+    listItems.forEach((item) => {
       observer.observe(item);
     });
+
+    const trigger = document.querySelector(".trigger");
+    observer.observe(trigger);
 
     return () => observer.disconnect();
   }, []);
@@ -38,20 +56,17 @@ function App() {
   return (
     <div className="container" ref={ref}>
       <ol>
-        <li className="list-item">"The only limit to our realization of tomorrow will be our doubts of today." – Franklin D. Roosevelt</li>
-        <li className="list-item">"Do what you can, with what you have, where you are." – Theodore Roosevelt</li>
-        <li className="list-item">"Don’t count the days, make the days count." – Muhammad Ali</li>
-        <li className="list-item">"Hardships often prepare ordinary people for an extraordinary destiny." – C.S. Lewis</li>
-        <li className="list-item">"Success is walking from failure to failure with no loss of enthusiasm." – Winston Churchill</li>
-        <li className="list-item">"It does not matter how slowly you go as long as you do not stop." – Confucius</li>
-        <li className="list-item">"What lies behind us and what lies before us are tiny matters compared to what lies within us." – Ralph Waldo Emerson</li>
-        <li className="list-item">"You miss 100% of the shots you don’t take." – Wayne Gretzky</li>
-        <li className="list-item">"Strive not to be a success, but rather to be of value." – Albert Einstein</li>
-        <li className="list-item">"Dream big and dare to fail." – Norman Vaughan</li>
-        <li className="list-item last">Loading...</li>
+        <ol>
+          {quotes.map((quote) => (
+            <li key={quote} className="list-item">
+              {quote}
+            </li>
+          ))}
+          <li className="trigger">Loading...</li>
+        </ol>
       </ol>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
