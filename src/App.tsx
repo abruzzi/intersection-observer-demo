3cbff80c11a83a49c6550ca3f8a0c9d6bc40c950
Juntao Qiu <juntao.qiu@gmail.com>
2024-12-21 15:32:11 +1100

    init the repo with a list of quotes

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="root">
+      <ol>
+        <li>"The only way to do great work is to love what you do." – Steve Jobs</li>
+        <li>"Success usually comes to those who are too busy to be looking for it." – Henry David Thoreau</li>
+        <li>"Don’t be afraid to give up the good to go for the great." – John D. Rockefeller</li>
+        <li>"Opportunities don't happen. You create them." – Chris Grosser</li>
+        <li>"I find that the harder I work, the more luck I seem to have." – Thomas Jefferson</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
